ce675582fd81084f2bcb3c971210d7015ec61abc
Efe-Roy <dakaraefe@gmail.com>
2024-03-25 11:10:33 +0100

    we

--- store/models.py
+++ store/models.py
@@ -70,7 +70,7 @@ class OrderItem(models.Model):
         return self.get_total_item_price()
 
 class Order(models.Model):
-    rider = models.OneToOneField(User, related_name='rider', on_delete=models.SET_NULL, blank=True, null=True)
+    rider = models.OneToOneField(User, related_name='rider', on_delete=models.SET_NULL, blank=True, null=True, limit_choices_to={'is_rider': True})
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, blank=True, null=True)
     ref_code = models.CharField(max_length=20, blank=True, null=True)
